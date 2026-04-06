18528d748b787e76e08eeb61a68edafd447ce7d1
wingit88 <compatiblewindows8@gmail.com>
2026-04-06 08:58:26 +0700

    remove ssg

--- src/pages/produk/[produk].tsx
+++ src/pages/produk/[produk].tsx
@@ -30,41 +30,41 @@ export default HalamanProduk;
 
 // Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses, dan akan mengambil data produk dari API sebelum merender halaman.
 {/*digunakan server-side rendering*/}
-// export async function getServerSideProps({ params }: { params: { produk: string } }) {
-//   const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
-//   const respone = await res.json();
-//   // console.log("Data produk yang diambil dari API:", respone);
-//   return {
-//     props: {
-//       product: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
-//     },
-//   };
-// }
+export async function getServerSideProps({ params }: { params: { produk: string } }) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+  const respone = await res.json();
+  // console.log("Data produk yang diambil dari API:", respone);
+  return {
+    props: {
+      product: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
+    },
+  };
+}
 
 {/*digunakan static-site generation*/}
-export async function getStaticPaths() {
-    const res = await fetch('http://localhost:3000/api/products')
-    const response = await res.json()
+// export async function getStaticPaths() {
+//     const res = await fetch('http://localhost:3000/api/products')
+//     const response = await res.json()
 
-    const paths = response.data.map((product: ProductType) => ({
-        params: { produk: product.id }
-    }))
-    // console.log("Paths yang dihasilkan untuk produk:", paths); // Debugging: Tampilkan paths yang dihasilkan
-    return {
-        paths,
-        fallback: false
-    }
-}
+//     const paths = response.data.map((product: ProductType) => ({
+//         params: { produk: product.id }
+//     }))
+//     // console.log("Paths yang dihasilkan untuk produk:", paths); // Debugging: Tampilkan paths yang dihasilkan
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
 
-export async function getStaticProps({params}:{params:{produk:string}}) {
-    const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
-    // const response: ProductType[] = await res.json();
-    const response: { data: ProductType[] } = await res.json();
+// export async function getStaticProps({params}:{params:{produk:string}}) {
+//     const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+//     // const response: ProductType[] = await res.json();
+//     const response: { data: ProductType[] } = await res.json();
 
-    // console.log("Data produk yang diambil dari API:", response);
-    return {
-        props: {
-            product: response.data,
-        }
-    }
-}
+//     // console.log("Data produk yang diambil dari API:", response);
+//     return {
+//         props: {
+//             product: response.data,
+//         }
+//     }
+// }
