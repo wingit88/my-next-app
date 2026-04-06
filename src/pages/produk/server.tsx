b459529c89475bc5ba2875fdd8151a1e1f0f8876
rockhubzz <compatiblewindows7@gmail.com>
2026-04-06 16:12:41 +0700

    fix invalid fetch url

--- src/pages/produk/server.tsx
+++ src/pages/produk/server.tsx
@@ -16,7 +16,7 @@ export default halamanProdukServer;
 // Fungsi getServerSideProps akan dipanggil setiap kali halaman ini diakses,
 // dan akan mengambil data produk dari API sebelum merender halaman.
 export async function getServerSideProps() {
-  const res = await fetch("${process.env.NEXT_PUBLIC_API_URL}/api/produk");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/produk`);
   const respone = await res.json();
   // console.log("Data produk yang diambil dari API:", respone);
   return {
